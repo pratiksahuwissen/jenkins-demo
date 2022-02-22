ab7e303716c358727bc6afde222ca5a7f6d62c32
Pratik Sahu <pratik.sahu@wissen.com>
2022-02-22 13:37:20 +0530

    changed main method to pass args

--- CalcExecutor.java
+++ CalcExecutor.java
@@ -2,16 +2,16 @@ import java.util.*;
 
 public class CalcExecutor{
 	public static void main(String args[]){
-		Scanner sc = new Scanner(System.in);
+		//Scanner sc = new Scanner(System.in);
 		Calculator calculator = new Calculator();
 		System.out.println("Enter a<SPACE>operator<SPACE>b");
-		String[] ab = sc.nextLine().split(" ");
-		int a = Integer.parseInt(ab[0]);
-		int b = Integer.parseInt(ab[2]);
+		//String[] ab = sc.nextLine().split(" ");
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[2]);
 		System.out.println("a : " + a + "  b : " + b);
-		if(ab[1].equals("+"))
+		if(args[1].equals("+"))
 			System.out.println("Result of addition is : " + calculator.add(a, b));
-		else if(ab[1].equals("-"))
+		else if(args[1].equals("-"))
 			System.out.println("Result of subtraction is : " + calculator.subtract(a, b));
 		
 	}
