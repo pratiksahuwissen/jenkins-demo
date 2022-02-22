396dc393640a9ec97980928b2757397c89b4a619
Pratik Sahu <pratik.sahu@wissen.com>
2022-02-22 14:03:56 +0530

    Removed sout

--- CalcExecutor.java
+++ CalcExecutor.java
@@ -3,7 +3,6 @@ import java.util.*;
 public class CalcExecutor{
 	public static void main(String args[]){
 		Calculator calculator = new Calculator();
-		System.out.println("Enter a<SPACE>operator<SPACE>b");
 		int a = Integer.parseInt(args[0]);
 		int b = Integer.parseInt(args[2]);
 		System.out.println("a : " + a + "  b : " + b);
